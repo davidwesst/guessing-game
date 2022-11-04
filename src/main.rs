09026e28c1a728784ad9d57ac40281bd3d2394ea
David Wesst <david@cocobokostudios.com>
2022-11-03 23:40:06 -0500

    comparing guess to RNG number

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
+	let answer = rand::thread_rng().gen_range(1..=100);
 	println!("I am thinking of a number. I bet you can guess it.");
+	println!("(DEBUG) Answer = {answer}");
 	println!("Try it! Enter a number: ");
 
 	let mut guess = String::new();
@@ -9,6 +13,14 @@ fn main() {
 	io::stdin()
 		.read_line(&mut guess)
 		.expect("Failed to read the input.");
+
+	let guess : u32 = guess.trim().parse().expect("You need to guess a number silly!");
+	
+match guess.cmp(&answer) {
+		Ordering::Less => println!("Too low!"),
+		Ordering::Greater => println!("Too high!"),
+		Ordering::Equal => println!("You got it!")
+	}
 
 	println!("You guessed: {guess}");
 }
